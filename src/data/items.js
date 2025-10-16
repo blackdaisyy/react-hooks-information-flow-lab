aae5e01eb85daa5f92798da0a29b816d6b1a278f
Peace Kuria <ephraimekuria@gmail.com>
2025-10-16 07:33:10 +0300

    flow-lab

--- src/data/items.js
+++ src/data/items.js
@@ -1,9 +1,24 @@
 const items = [
-  { id: 1, name: "Yogurt", category: "Dairy" },
-  { id: 2, name: "Pomegranate", category: "Produce" },
-  { id: 3, name: "Lettuce", category: "Produce" },
-  { id: 4, name: "String Cheese", category: "Dairy" },
-  { id: 5, name: "Cookies", category: "Dessert" },
+  {
+    id: 1,
+    name: "Milk",
+    category: "Dairy",
+  },
+  {
+    id: 2,
+    name: "Yogurt",
+    category: "Dairy",
+  },
+  {
+    id: 3,
+    name: "Apples",
+    category: "Produce",
+  },
+  {
+    id: 4,
+    name: "Cookies",
+    category: "Dessert",
+  },
 ];
 
 export default items;
